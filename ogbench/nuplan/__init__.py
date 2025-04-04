26b07fc0f084d1497d00d39dd951154d9fa12b62
Achyuth Krishnan Vivek <achyuthkv76@berkeley.edu>
2025-04-04 16:41:13 -0700

    fixed import

--- ogbench/nuplan/__init__.py
+++ ogbench/nuplan/__init__.py
@@ -3,6 +3,7 @@ from gymnasium.envs.registration import register
 from ogbench.nuplan.env import NuplanEnv
 from ogbench.nuplan.config import get_nuplan_config
 from ogbench.nuplan.dataset import NuplanDataset
+from ogbench.nuplan.loader import NuplanLoader
 
 # Register nuplan environment
 register(
@@ -19,4 +20,19 @@ register(
     kwargs={'config': get_nuplan_config(), 'visual': True}
 )
 
-__all__ = ['NuplanEnv', 'get_nuplan_config', 'NuplanDataset'] 
+__all__ = ['NuplanEnv', 'get_nuplan_config', 'NuplanDataset']
+
+class NuplanLoader:
+    def __init__(self, data_dir, config):
+        self.data_dir = data_dir
+        self.config = config
+
+    def load_dataset(self, dataset_name):
+        # Placeholder for loading logic
+        return {
+            'observations': [],
+            'actions': [],
+            'rewards': [],
+            'next_observations': [],
+            'terminals': []
+        } 
