12373e4f8804f2d25981e018ec8ca90cc5ed88d6
Achyuth Krishnan Vivek <achyuthkv76@berkeley.edu>
2025-04-05 19:12:09 -0700

    fixed imports

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,21 +1,21 @@
-from agents.crl import CRLAgent
-from agents.gcbc import GCBCAgent
-from agents.gciql import GCIQLAgent
-from agents.hiql import HIQLAgent
-from agents.ppo import PPOAgent
-from agents.qrl import QRLAgent
-from agents.sac import SACAgent
-from agents.cmd import CMDAgent
-from agents.tra import TRAAgent
+from impls.agents.crl import CRLAgent
+from impls.agents.gcbc import GCBCAgent
+from impls.agents.gciql import GCIQLAgent
+from impls.agents.hiql import HIQLAgent
+#from impls.agents.ppo import PPOAgent
+from impls.agents.qrl import QRLAgent
+from impls.agents.sac import SACAgent
+from impls.agents.cmd import CMDAgent
+#from impls.agents.tra import TRAAgent
 
 agents = dict(
     crl=CRLAgent,
     gcbc=GCBCAgent,
     gciql=GCIQLAgent,
     hiql=HIQLAgent,
-    ppo=PPOAgent,
+    #ppo=PPOAgent,
     qrl=QRLAgent,
     sac=SACAgent,
     cmd=CMDAgent,
-    tra=TRAAgent,
+    #tra=TRAAgent,
 )
