ed8442dfd39ee5d18ea6d3786e04bb95efee7f06
Vivek Myers <vivek.myers@gmail.com>
2025-09-07 01:34:54 -0700

    add agent import

---
+++ impls/__init__.py
@@ -0,0 +1,24 @@
+from agents.clearn import ClearnAgent
+from agents.cmd import CMDAgent
+from agents.crl import CRLAgent
+from agents.gcbc import GCBCAgent
+from agents.gciql import GCIQLAgent
+from agents.hiql import HIQLAgent
+from agents.ppo import PPOAgent
+from agents.qrl import QRLAgent
+from agents.sac import SACAgent
+from agents.tra import TRAAgent
+
+
+agents = dict(
+    crl=CRLAgent,
+    gcbc=GCBCAgent,
+    gciql=GCIQLAgent,
+    hiql=HIQLAgent,
+    ppo=PPOAgent,
+    qrl=QRLAgent,
+    sac=SACAgent,
+    cmd=CMDAgent,
+    tra=TRAAgent,
+    clearn=ClearnAgent,
+)
